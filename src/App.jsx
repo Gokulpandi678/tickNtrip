c5d33f60adac929057110f88b8a6ab2f443a25df
Gokulpandi678 <gokulpandi678@gmail.com>
2025-10-07 17:41:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,8 @@ function App() {
         <li>Jotai</li>
         <li>lucide-react</li>
         <li>tailwind css</li>
+        <li>font libraries</li>
       </ul>
-      <div className="p-8 space-y-8 bg-gray-50 min-h-screen">
-      <div className="font-outfit">
-        <h1 className="text-4xl font-bold text-blue-700"> Font</h1>
-        <p className="text-gray-700 text-lg">
-          Clean, modern, and geometric. Great for professional brands like “TickNTrip”.
-        </p>
-      </div>
-
-      <div className="font-poppins">
-        <h1 className="text-4xl font-bold text-green-700"> Font</h1>
-        <p className="text-gray-700 text-lg">
-          Stylish and friendly, perfect for headings with a tech or travel vibe.
-        </p>
-      </div>
-
-      <div className="font-inter">
-        <h1 className="text-4xl font-bold text-purple-700"> Font</h1>
-        <p className="text-gray-700 text-lg">
-          Minimal and highly readable — ideal for paragraph text and UI elements.
-        </p>
-      </div>
-    </div>
     </div>
   );
 }
